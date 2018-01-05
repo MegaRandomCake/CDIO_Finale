6c923ff8aa935bfa48fac46db8bb63989d6e00bf
Kasper <Kasper@82.211.220.90>
2018-01-05 13:17:00 +0100

    NullPointerException on F1 test.

--- src/test/DiceToField.java
+++ src/test/DiceToField.java
@@ -14,16 +14,19 @@ class DiceToField {
 	@Before
 	public void setUp() throws Exception {
 		cup = new Cup();
+		player = new Player();
 	}
 
 	
 
 	@Test
 	public void testDiceToField() {
+		player.setField(1);
 		cup.rollCup();
-		int actual = cup.getEyes();
-		int expected = player.getField()+1;
+		int actual = cup.getEyes()+1;
+		int expected = player.getField();
 		assertEquals(actual, expected);
 	}
 
 }
+ 
