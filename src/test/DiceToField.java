f3744ff326754e17b7599a479e5442c6caf02930
denzildk <hedegaard.cornelius@gmail.com>
2018-01-06 00:47:30 +0100

    minor fix
    initialiserede cup og player i testen.

--- src/test/DiceToField.java
+++ src/test/DiceToField.java
@@ -13,19 +13,21 @@ class DiceToField {
 
 	@Before
 	public void setUp() throws Exception {
-		cup = new Cup();
-		player = new Player();
+		this.cup = new Cup();
+		this.player = new Player();
 	}
 
 	
 
 	@Test
 	public void testDiceToField() {
-		player.setField(1);
-		cup.rollCup();
-		player.addToField(cup.getEyes());
-		int actual = cup.getEyes();
-		int expected = player.getField();
+		this.cup = new Cup();
+		this.player = new Player();
+		this.player.setField(1);
+		this.cup.rollCup();
+		this.player.addToField(this.cup.getEyes());
+		int actual = this.cup.getEyes();
+		int expected = this.player.getField();
 		assertEquals(actual+1, expected);
 	}
 
