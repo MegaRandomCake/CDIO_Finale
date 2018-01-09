3e4ba9630fae7db47c112534a40fcf4d9ba93eb9
Kasper <Kasper@82.211.220.90>
2018-01-09 03:36:11 +0100

    Created empty JUnit for testing for extra turn if doubles apear.

---
+++ src/test/ExtraTurnTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import entity.Cup;
+
+public class ExtraTurnTest {
+	
+	Cup cup;
+
+	@Before
+	public void setUp() throws Exception {
+		Cup cup = new Cup();
+	}
+
+	@Test
+	public void testDoubles() {
+	
+	}
+
+}
